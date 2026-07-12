eb33d631fb936861d902f8551baaf992864a283d
Bipin Verma <dev.vermabipin@gmail.com>
2026-07-12 13:01:51 +0530

    fix: Extract PasswordEncoder to separate configuration class
    This resolves an UnsatisfiedDependencyException during integration tests where HotelSecurityConfig is intentionally disabled.

--- src/main/java/com/cn/hotelDemo/config/HotelSecurityConfig.java
+++ src/main/java/com/cn/hotelDemo/config/HotelSecurityConfig.java
@@ -46,11 +46,6 @@ public class HotelSecurityConfig {
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception 
 	{
 		http
